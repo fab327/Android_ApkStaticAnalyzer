63d58109068cba6a1b863ab376ff52e5e3b72348
f@b Macbook <fab327@hotmail.com>
2015-11-11 16:46:28 -0500

    Implemented mac portion of dex2Jar & apktool

--- src/StaticAnalyzer.java
+++ src/StaticAnalyzer.java
@@ -1,18 +1,26 @@
+import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by fab on 11/10/2015.
+ * Created by fab on 11/10/2015
  */
 public class StaticAnalyzer {
 
-    private enum OS { Windows, OSX }
+    private enum OS {Windows, OSX}
+
     private OS currentOs;
-    private String javaHome;
+    private String javaHome, apkName, apkLocation, dexDirectory;
 
     public static void main(String[] args) {
         StaticAnalyzer staticAnalyzer = new StaticAnalyzer();
         staticAnalyzer.determineOS();
         staticAnalyzer.determineJavaHome();
+
+        //Choose the file to analyze
+        staticAnalyzer.chooseApk();
 
         //Test calling out the tools
         staticAnalyzer.doApkTool();
@@ -23,7 +31,7 @@ public class StaticAnalyzer {
         String os = System.getProperty("os.name");
         if (os.contains("Windows"))
             currentOs = OS.Windows;
-        else
+        else if (os.contains("Mac"))
             currentOs = OS.OSX;
     }
 
@@ -32,10 +40,13 @@ public class StaticAnalyzer {
         if (javaHome == null) {
             String path = System.getenv("PATH");
             String[] pathArray = path.split(";");
-            for (String item: pathArray) {
+            for (String item : pathArray) {
                 if (item.contains("javapath"))
                     javaHome = item;
             }
+        }
+        if (javaHome == null && currentOs == OS.OSX) {
+            javaHome = "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin";
         }
         switch (currentOs) {
             case Windows:
@@ -47,15 +58,39 @@ public class StaticAnalyzer {
         }
     }
 
+    private synchronized void chooseApk() {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setExtendedState(JFrame.ICONIFIED);
+        frame.setExtendedState(JFrame.NORMAL);
+
+        JFileChooser fc = new JFileChooser();
+        fc.setCurrentDirectory(new File(System.getProperty("user.dir")));
+        if (JFileChooser.APPROVE_OPTION == fc.showOpenDialog(null)) {
+            frame.setVisible(false);
+            apkName = fc.getSelectedFile().getName().replace(".apk", "");
+            apkLocation = fc.getSelectedFile().getAbsolutePath();
+            frame.dispose();
+        } else {
+            System.out.println("Next time select a file.");
+            System.exit(1);
+        }
+    }
+
     private synchronized void doApkTool() {
+        List<String> arguments = new ArrayList<>();
+        arguments.add(javaHome);
+        arguments.add("-jar");
+        arguments.add("./tools/apktool.jar");
+        arguments.add("d");
+        arguments.add(apkLocation);
+        arguments.add("-f");
+        arguments.add("-o");
+        arguments.add("output/" + apkName);
+
         switch (currentOs) {
             case Windows:
-                ProcessBuilder pb = new ProcessBuilder(javaHome, "-jar", "./tools/apktool.jar", "d", "./samples/FindAndCallSpyware.apk");
-//                Map<String, String> env = pb.environment();
-//                env.put("VAR1", "myValue");
-//                env.remove("OTHERVAR");
-//                env.put("VAR2", env.get("VAR1") + "suffix");
-//                pb.directory(new File("./output"));
+                ProcessBuilder pb = new ProcessBuilder(arguments);
                 try {
                     Process p = pb.start();
                     p.waitFor();
@@ -64,15 +99,30 @@ public class StaticAnalyzer {
                 }
                 break;
             case OSX:
-
+                pb = new ProcessBuilder(arguments);
+                try {
+                    Process p = pb.start();
+                    p.waitFor();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
                 break;
         }
     }
 
     private synchronized void doDex2Jar() {
+        createFolderForDex2Jar();
+
+        List<String> arguments = new ArrayList<>();
         switch (currentOs) {
             case Windows:
-                ProcessBuilder pb = new ProcessBuilder("./tools/dex2jar/d2j-dex2jar.bat", "./samples/TGLoader.apk");
+                arguments.add("./tools/dex2jar/d2j-dex2jar.bat");
+                arguments.add(apkLocation);
+                arguments.add("-f");
+                arguments.add("-o");
+                arguments.add(dexDirectory);
+
+                ProcessBuilder pb = new ProcessBuilder(arguments);
                 try {
                     Process p = pb.start();
                     p.waitFor();
@@ -81,8 +131,42 @@ public class StaticAnalyzer {
                 }
                 break;
             case OSX:
+                arguments.add("sh");
+                arguments.add("./tools/dex2jar/d2j-dex2jar.sh");
+                arguments.add(apkLocation);
+                arguments.add("-f");
+                arguments.add("-o");
+                arguments.add(dexDirectory);
 
+                ProcessBuilder permissionProcess = new ProcessBuilder("chmod", "+x", "./tools/dex2jar/d2j_invoke.sh");
+                pb = new ProcessBuilder(arguments);
+                try {
+                    Process permP = permissionProcess.start();
+                    permP.waitFor();
+                    Process p = pb.start();
+                    p.waitFor();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
                 break;
+        }
+    }
+
+    /**
+     * Helper method for doDex2Jar
+     */
+    private synchronized void createFolderForDex2Jar() {
+        dexDirectory = "./output/" + apkName + "Classes";
+        List<String> arguments = new ArrayList<>();
+        arguments.add("mkdir");
+        arguments.add(dexDirectory);
+
+        ProcessBuilder pb = new ProcessBuilder(arguments);
+        try {
+            Process p = pb.start();
+            p.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
